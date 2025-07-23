22b613602fffcc87bdc0b18cb233643df8668e8a
Dev <tasin17@hotmail.com>
2025-07-23 14:14:45 +0100

    fixed weight init

--- models.py
+++ models.py
@@ -263,7 +263,7 @@ class DiT(nn.Module):
         nn.init.normal_(self.t_embedder.mlp[2].weight, std=0.02)
 
         # Zero-out adaLN modulation layers in DiT blocks:
-        for block in self.person_blocks:
+        for block in self.mains:
             nn.init.constant_(block.adaLN_modulation[-1].weight, 0)
             nn.init.constant_(block.adaLN_modulation[-1].bias, 0)
 
